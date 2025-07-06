bc0dd1f11b6d65e88ca3672a06424366dec8b5a3
mhdrdn122 <mhdrdn122@gmail.com>
2025-07-06 14:25:54 +0300

    clean code

--- src/Components/Orders/OrdersContainer.jsx
+++ src/Components/Orders/OrdersContainer.jsx
@@ -1,135 +1,18 @@
-import { useState, useEffect } from "react";
-import {
-  useConfirmOneOrderMutation,
-  useGetOrdersQuery,
-} from "../../RtkQuery/Slice/Orders/OrdersApi";
-import {
-  useGetCitiesQuery,
-  useGetActiveRegionsByCityMutation,
-} from "../../RtkQuery/Slice/CitiesAndRegions/CitiesAndRegionsApi";
+
 import { DynamicTable } from "../../utils/Tables/DynamicTable";
 import { OrderFieldsShow } from "../../utils/Dialogs/Data/Show/OrderFieldsShow";
 import DialogShow from "../../utils/Dialogs/DialogShow/DialogShow";
 import { OrdersColumns } from "../../utils/Tables/ColumnsTable/OrdersColumns";
-import LoadingGet from "../../utils/Loading/LoadingGet/LoadingGet";
-import { showToast } from "../../utils/Notifictions/showToast";
 import { DeleteDialog } from "../../utils/Dialogs/DeleteDialog/DeleteDialog";
 import { DialogEditOrders } from "../../utils/Dialogs/EditAddDialog/Edit/DialogEditOrders";
 import OrdersPdf from "./OrdersPdf";
-import { Loader, Loader2Icon } from "lucide-react";
+import { Loader } from "lucide-react";
 import { Permissions } from "../../Static/StaticData";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { HistoryOrdersColumns } from "../../utils/Tables/ColumnsTable/HistoryOrdersColumns";
 import useMangeOrders from "../../hooks/useMangeOrders";
 
 const OrdersContainer = () => {
-
-  // const [activeTab, setActiveTab] = useState("orders");
-
-  // const [orderType, setOrderType] = useState("");
-  // const [cityId, setCityId] = useState("");
-  // const [regionId, setRegionId] = useState("");
-  // const [actionDate, setActionDate] = useState("");
-  // const [regions, setRegions] = useState([]);
-
-  // const { data: cities, isLoading: isCitiesLoading } = useGetCitiesQuery();
-  // const [getActiveRegionsByCity] = useGetActiveRegionsByCityMutation();
-
-  // const queryParams = {
-  //   ...(orderType === "both" && { type: [3] }),
-  //   ...(orderType === "release" && { type: [1] }),
-  //   ...(orderType === "installation" && { type: [2] }),
-  //   ...(cityId && { city_id: cityId }),
-  //   ...(regionId && { region_id: regionId }),
-  //   ...(actionDate && { action_date: actionDate }),
-  //   ...( { status: false }),
-  // };
-
-  // const { data, isLoading, isFetching } = useGetOrdersQuery(queryParams, {
-  //   refetchOnMountOrArgChange: true,
-  // });
-
-  // const {
-  //   data: history,
-  //   isLoading: isLoadingHistory,
-  //   isFetching: isFetchingHistory,
-  // } = useGetOrdersQuery(
-  //   {
-  //     status: true,
-  //   },
-  //   {
-  //     refetchOnMountOrArgChange: true,
-  //   }
-  // );
-  // const [confirmOrder, { isLoading: iLoadingConfirm }] =
-  //   useConfirmOneOrderMutation();
-
-  // const [openShow, setOpenShow] = useState(false);
-  // const [openConfirm, setOpenConfirm] = useState(false);
-  // const [openEdit, setOpenEdit] = useState(false);
-  // const [isConfirmAction, setIsConfirmAction] = useState(true);
-  // const [selectedOrder, setSelectedOrder] = useState(null);
-
-  // // Fetch regions when city changes
-  // useEffect(() => {
-  //   const fetchRegions = async () => {
-  //     if (cityId) {
-  //       try {
-  //         const response = await getActiveRegionsByCity(cityId).unwrap();
-  //         setRegions(response || []);
-  //         setRegionId(""); // Reset region when city changes
-  //       } catch (error) {
-  //         showToast("error", "فشل في جلب المناطق");
-  //       }
-  //     } else {
-  //       setRegions([]);
-  //       setRegionId("");
-  //     }
-  //   };
-  //   fetchRegions();
-  // }, [cityId, getActiveRegionsByCity]);
-
-  // const handleEdit = (order) => {
-  //   setSelectedOrder(order);
-  //   setOpenEdit(true);
-  // };
-
-  // const handleShow = (order) => {
-  //   setSelectedOrder(order);
-  //   setOpenShow(true);
-  // };
-
-  // const handleConfirmOrUnconfirm = async () => {
-  //   try {
-  //     const status = isConfirmAction ? 1 : 0;
-  //     await confirmOrder({ status, id: selectedOrder.id }).unwrap();
-  //     showToast(
-  //       "success",
-  //       isConfirmAction ? "تم تأكيد الطلب بنجاح" : "تم إلغاء تأكيد الطلب بنجاح"
-  //     );
-  //   } catch (err) {
-  //     showToast(
-  //       "error",
-  //       `حدث خطأ أثناء ${isConfirmAction ? "تأكيد" : "إلغاء تأكيد"} الطلب: ${
-  //         err.data?.message || "خطأ غير معروف"
-  //       }`
-  //     );
-  //   }
-  //   setOpenConfirm(false);
-  // };
-
-  // const onConfirmOrder = (row) => {
-  //   setSelectedOrder(row);
-  //   setIsConfirmAction(true);
-  //   setOpenConfirm(true);
-  // };
-
-  // const onUnconfirmOrder = (row) => {
-  //   setSelectedOrder(row);
-  //   setIsConfirmAction(false);
-  //   setOpenConfirm(true);
-  // };
-
   const 
         {
           setActiveTab , 
